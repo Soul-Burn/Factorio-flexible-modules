a32ff50afcad04db9dceb234dfb8f9f4652e9023
Soul-Burn <sugoibaka@gmail.com>
2022-01-15 11:11:20 +0200

    Initial release

---
+++ settings.lua
@@ -0,0 +1,14 @@
+data:extend({
+  {
+    type = "bool-setting",
+    name = "fm-disable-direct-recipes",
+    setting_type = "startup",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "fm-disable-blank-recipes",
+    setting_type = "startup",
+    default_value = false,
+  },
+})
